158c980c7f71a5bbc87b3661955b3aa5a97ba4ec
Wenard Santos <wenardken57@gmail.com>
2025-03-20 10:36:08 +0800

    change logo redirects to respective role

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,9 +5,16 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
+                    @if (auth()->user()->role === 'customer')
+                        <a href="{{ route('customer.dashboard') }}">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        </a>
+                    @elseif (auth()->user()->role === 'admin')
+                        <a href="{{ route('admin.dashboard') }}">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        </a>
+                    @endif
+
                 </div>
 
                 <!-- Navigation Links -->
