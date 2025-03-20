3867da6d9e48893d8c29b8253518970ad9a4a0a9
Wenard Santos <wenardken57@gmail.com>
2025-03-20 15:43:43 +0800

    add event addons table

---
+++ database/migrations/2025_03_20_074124_create_event_addons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_addons', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('item_name', 255)
+            ->nullable(false);
+
+            $table->foreignId('event_package_id')
+            ->constrained('event_packages')
+            ->cascadeOnDelete();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_addons');
+    }
+};
